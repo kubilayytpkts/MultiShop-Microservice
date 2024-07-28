812e2aae64b9e8a2366274e3085b606d12693443
kubilayytpkts <119957098+kubilayytpkts@users.noreply.github.com>
2024-07-28 20:47:59 +0300

    Update 28.07.24

--- Services/Order/Presantation/MultiShop.Order.WebApi/Controllers/OrderingsController.cs
+++ Services/Order/Presantation/MultiShop.Order.WebApi/Controllers/OrderingsController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailsResults;
+using MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommand;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+using MultiShop.Order.Application.Features.Mediator.Results;
 
 namespace MultiShop.Order.WebApi.Controllers
 {
@@ -7,5 +12,46 @@ namespace MultiShop.Order.WebApi.Controllers
     [ApiController]
     public class OrderingsController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        public OrderingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrdering(CreateOrderingCommand ordering)
+        {
+            await _mediator.Send(ordering);
+            return Ok("Ekleme işlemi başarılı");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrdering(UpdateOrderingCommand ordering)
+        {
+            await _mediator.Send(ordering);
+            return Ok("Güncelleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteOrdering(int id)
+        {
+            await _mediator.Send(new RemoveOrderingCommmand(id));
+            return Ok("Silme işlemi başarılı");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrderings()
+        {
+            var value = await _mediator.Send(new GetOrderingQuery());
+            return Ok(value);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderingById(int id)
+        {
+            var value = await _mediator.Send(new GetOrderingByIDQuery(id));
+            return Ok(value);
+        }
+        //REGİSTİR
     }
 }
