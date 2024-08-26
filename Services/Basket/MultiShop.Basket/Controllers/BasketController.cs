5288ac891fe3c9b743de2ef951f4c0a8dd915b04
kubil <kubil@KUBILAY>
2024-08-27 00:47:17 +0300

    Controller Update 27.08.24

--- Services/Basket/MultiShop.Basket/Controllers/BasketController.cs
+++ Services/Basket/MultiShop.Basket/Controllers/BasketController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.Basket.Dtos;
+using MultiShop.Basket.LoginServices;
+using MultiShop.Basket.Services;
 
 namespace MultiShop.Basket.Controllers
 {
@@ -7,5 +10,34 @@ namespace MultiShop.Basket.Controllers
     [ApiController]
     public class BasketController : ControllerBase
     {
+        private readonly IBasketService _basketService;
+        private readonly IloginService _loginService;
+
+        public BasketController(IBasketService basketService, IloginService loginService)
+        {
+            _basketService = basketService;
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveMyBasket(BasketTotalItemDto basketTotalItemDto)
+        {
+            basketTotalItemDto.UserId = _loginService.GetUserId;
+            _basketService.SaveBasket(basketTotalItemDto);
+            return Ok("Sepet Güncelleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteMyBasket()
+        {
+            _basketService.DeleteBasket(_loginService.GetUserId);
+            return Ok("Sepet Silme işlemi başarılı");
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetMyBasket()
+        {
+            var userBasket = _basketService.GetBasket(_loginService.GetUserId);
+            return Ok(userBasket);    
+        }
     }
 }
