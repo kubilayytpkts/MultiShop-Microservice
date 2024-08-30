a67f20bdd65e4507d8764a257a608e1d720be6c4
kubil <kubil@KUBILAY>
2024-08-30 15:24:43 +0300

    update 30.08.24

---
+++ MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentView.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _FooterUILayoutComponentView:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
