667d4c868b9263ad21475d0058bd7e88145a5419
kubil <kubil@KUBILAY>
2024-08-31 21:28:33 +0300

    update 31.08.24 3/3

---
+++ MultiShop.WebUI/Areas/Admin/Controllers/LayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
---
+++ MultiShop.WebUI/ViewComponents/ContactPageComponents/_ContactPageMapeComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ContactPageComponents
+{
+    public class _ContactPageMapeComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
