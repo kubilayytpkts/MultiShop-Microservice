6742cfcc3535c8331bc783241d6d9a8e4dbc3457
kubil <kubil@KUBILAY>
2024-08-31 17:28:45 +0300

    update 31.08.24 2/3

---
+++ MultiShop.WebUI/ViewComponents/ShoppingCartComponents/_ProductListForShoppingComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCartComponents
+{
+    public class _ProductListForShoppingComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
