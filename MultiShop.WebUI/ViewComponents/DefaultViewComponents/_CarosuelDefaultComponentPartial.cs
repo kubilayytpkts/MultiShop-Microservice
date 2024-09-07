165c15117fcc8a24fc3cef96d8658c20254f5d81
kubil <kubil@KUBILAY>
2024-09-07 14:35:32 +0300

    update 07.09.24 part 2

--- MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CarosuelDefaultComponentPartial.cs
+++ MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CarosuelDefaultComponentPartial.cs
@@ -1,11 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using MultiShop.Dtos.FeatureSliderDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _CarosuelDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CarosuelDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/FeatureSlider");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var data = await responseMessage.Content.ReadAsStringAsync();
+                var deserializeData = JsonConvert.DeserializeObject<List<ResultFeatureSliderDto>>(data);
+
+                List<ResultFeatureSliderDto> getAllActiveFeatureSlider=new List<ResultFeatureSliderDto>();
+
+                foreach (var item in deserializeData)
+                {
+                    if(item.Status == false)
+                        getAllActiveFeatureSlider.Add(item);
+                }
+
+                return View(getAllActiveFeatureSlider);
+            }
             return View();
         }
     }
